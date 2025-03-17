5c8c063f55c16f4417236f95069637306b7a1326
Alexandre Kerber <alexkerber54190@gmail.com>
2025-03-17 14:40:33 +0100

    :construction: WIP

--- src/main/java/fr/archimon/ArchiMon/domain/models/ArchiMon.java
+++ src/main/java/fr/archimon/ArchiMon/domain/models/ArchiMon.java
@@ -18,14 +18,29 @@ public class ArchiMon {
     @Column(name = "nom")
     private String nom;
 
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "id_type1")
+    private int id_type1;
+
+    @Column(name = "id_type2")
+    private int id_type2;
+
     @Column(name = "pv")
     private int pv;
 
     @Column(name = "attaque")
-    private int attaque;
+    private int atk;
+
+    @Column(name = "sp_attaque")
+    private int sp_atk;
 
     @Column(name = "defense")
-    private int defense;
+    private int def;
+
+    @Column(name = "sp_defense")
+    private int sp_def;
 
     @Column(name = "vitesse")
     private int vitesse;
